d365866861561b4293c9e6953c45d5b21029ffd1
Vadikvdm <vadikvdm@gmail.com>
2022-07-04 12:40:34 +0300

    modified

--- 7/js/index.js
+++ 7/js/index.js
@@ -2,27 +2,27 @@ const ukraine = { tax: 0.195, middleSalary: 1789, vacancies: 11476 };
 const latvia = { tax: 0.25, middleSalary: 1586, vacancies: 3921 };
 const litva = { tax: 0.15, middleSalary: 1509, vacancies: 1114 };
 
-function getMyTaxes() {
-    return this.tax * this.middleSalary;
+function getMyTaxes(salary) {
+    return this.tax * salary;
 }
-const inform = getMyTaxes.call(ukraine);
+const inform = getMyTaxes.call(ukraine, 1500);
 console.log(`Податок який заплатить IT-спеціаліст в Україні ${inform}`);
 //////////////////////////////////
 function getMiddleTaxes() {
-     this.tax * this.middleSalary;
+     return this.tax * this.middleSalary;
 };
-const informs = getMyTaxes.call(ukraine);
-const informs2 = getMyTaxes.call(latvia);
-const informs3 = getMyTaxes.call(litva);
+const informs = getMiddleTaxes.call(ukraine);
+const informs2 = getMiddleTaxes.call(latvia);
+const informs3 = getMiddleTaxes.call(litva);
 
 console.log(`У середньому податок платять IT-спеціалісти ${Math.floor(informs + informs2 + informs3 / 3)}`);
 ////////////////////////////////////////////
 function getTotalTaxes() {
-    this.tax * this.middleSalary * vacancies;
+    return this.tax * this.middleSalary * this.vacancies;
 }
-const infor = getMyTaxes.call(ukraine);
-const infor2 = getMyTaxes.call(latvia);
-const infor3 = getMyTaxes.call(litva);
+const infor = getTotalTaxes.call(ukraine);
+const infor2 = getTotalTaxes.call(latvia);
+const infor3 = getTotalTaxes.call(litva);
 console.log(`Bсього податків платять IT-спеціалісти в Україні ${infor}, Латвії ${infor2}, Литві ${infor3}`);
 ////////////////////////////////////////////////
 const mySalary = {};
