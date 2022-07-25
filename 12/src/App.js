6e4547f92d4cfbf644834419e96a55bd55d80f26
Vadikvdm <vadikvdm@gmail.com>
2022-07-25 17:36:41 +0300

    react

---
+++ 12/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Constanta from './Constanta';
+import Post from './Post';
+
+
+
+function App() {
+  return (
+    <div className="App">
+        
+        <Post />
+        
+      
+    </div>
+  );
+}
+
+export default App;
